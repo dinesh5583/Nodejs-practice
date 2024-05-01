8be746d2a5f21b8d6894e8fb0152aa63f412a5da
Dinesh Mehra <dineshsinghmehra09@gmail.com>
2024-05-01 23:41:06 +0530

    express router

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ let express = require('express')
 let bodyParse = require('body-parser')
 
 const app=express();
-app.use(bodyParse.urlencoded({extended:false}))
-app.use('/add-product',(req,res,next)=>{
-    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>")
+
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop')
+
+app.use(bodyParse.urlencoded({extended:true}))
+
+app.use(adminRoutes)
+app.use(shopRoutes)
+
+//404 Page
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    // console.log('<h1>Hello From Express</h1>');
-    res.send('<h1>Hello From Express</h1>')
-})
+
+// app.use('/add-product',(req,res,next)=>{
+//     res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>")
+// })
+// app.use('/product',(req,res,next)=>{
+//     console.log(req.body);
+//     res.redirect('/')
+// })
+// app.use('/',(req,res,next)=>{
+//     // console.log('<h1>Hello From Express</h1>');
+//     res.send('<h1>Hello From Express</h1>')
+// })
 // app.use((req,res,next)=>{
 //     console.log('In The Middlwware');
 //     next()
